5314a534519cc98597177a83478532689461f633
dogman-joe <joseph.hemann15@gmail.com>
2024-04-07 14:57:49 -0700

    DOG LOG: add missing file to hello

---
+++ src/libdogs/hellodog/plat/rpi4b/hellodog.c
@@ -0,0 +1,18 @@
+#include "types.h"
+
+extern int dogprint(char *c);
+
+void process(u32 num) {
+  int a = 0;
+
+  while (1) {
+    for (int i = 0; i < 0x80000; i++)
+      a++;
+    for (int i = 0; i < num + 1; i++)
+      dogprint("proc");
+      dogprint("\r\n");
+  }
+
+  return;
+}
+
